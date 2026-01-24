d46434080fa860be88a923660c8ec13c5581e895
mkay-martin <e01634081@student.tuwien.ac.at>
2026-01-24 13:13:34 +0100

    commit

--- SOMToolbox/somtoolbox.py
+++ SOMToolbox/somtoolbox.py
@@ -19,22 +19,22 @@ import holoviews as hv
 from holoviews import opts
 from holoviews.streams import Pipe, Buffer
 
-from controls.controllers import MainController
+from .controls.controllers import MainController
 hv.extension('bokeh')
 
-from visualizations.complane import ComponentPlane
-from visualizations.dmatrix import DMatrix
-from visualizations.hithistogram import HitHist
-from visualizations.sdh import SDH
-from visualizations.qerror import QError
-from visualizations.umatrix import UMatrix
-from visualizations.upmatrix import UStar_PMatrix
-from visualizations.neighbourhood_graph import NeighbourhoodGraph
-from visualizations.clustering import Clustering
-from visualizations.metromap import MetroMap
-from visualizations.piechart import PieChart
-from visualizations.chessboard import Chessboard
-from visualizations.time_series import TimeSeries
+from .visualizations.complane import ComponentPlane
+from .visualizations.dmatrix import DMatrix
+from .visualizations.hithistogram import HitHist
+from .visualizations.sdh import SDH
+from .visualizations.qerror import QError
+from .visualizations.umatrix import UMatrix
+from .visualizations.upmatrix import UStar_PMatrix
+from .visualizations.neighbourhood_graph import NeighbourhoodGraph
+from .visualizations.clustering import Clustering
+from .visualizations.metromap import MetroMap
+from .visualizations.piechart import PieChart
+from .visualizations.chessboard import Chessboard
+from .visualizations.time_series import TimeSeries
 from skimage.transform import resize
 
 OBJECTS_CLASSES = [ComponentPlane, HitHist, UMatrix, DMatrix, UStar_PMatrix, 
